13b2e6b6ec3bf7e268f55c704768056d9190b297
zhenzheHz <109124xdf@gmail.com>
2024-07-05 20:10:44 +0800

    add arrange.cpp

---
+++ arrange.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    #define print(i) cout<<i<<'\n'
+    print(c);
+    print(a);
+    print(b);
+}
+

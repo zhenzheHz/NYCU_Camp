7610c1908a24fa6b6487e78a79b8347799875263
zhenzheHz <109124xdf@gmail.com>
2024-07-05 20:08:20 +0800

    add sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int input1, input2;
+    cout << "Input two integers: ";
+    cin >> input1 >> input2;
+    cout << "\nThe sum of the two integers is " << input1 + input2 << ".\n";
+    return 0;
+}
+

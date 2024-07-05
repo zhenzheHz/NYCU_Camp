06379b78185f31ab58d792c687e83c5d8f9dd51b
zhenzheHz <109124xdf@gmail.com>
2024-07-05 20:07:13 +0800

    add integar.cpp

---
+++ integar.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int input;
+    cout << "Input an integer: ";
+    cin >> input;
+    cout << "\nThe integer you input is " << input << "\n";
+    return 0;
+}
